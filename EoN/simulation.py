90602c7ec26a20f4d2c8c84e5906ca454297e7f6
Mpkosh <koshk.mp@gmail.com>
2025-08-28 14:49:40 +0300

    .

--- EoN/simulation.py
+++ EoN/simulation.py
@@ -4006,6 +4006,7 @@ def Gillespie_simple_contagion(G, spontaneous_transition_graph,
         t += delay
         
         # ______ Switching mechanism
+        print(frac_pop)
         if frac_pop == 'Infected':
             chosen_pop = data['I'][-1]
             
@@ -4017,6 +4018,8 @@ def Gillespie_simple_contagion(G, spontaneous_transition_graph,
                 # Inc_t = (E_t-1 - E_t) - (S_t - S_t-1)
                 chosen_pop = (data['E'][-2] - data['E'][-1]
                              ) - (data['S'][-1] - data['S'][-2])
+        print(chosen_pop)
+        print(data['E'])
         
         if chosen_pop > pop*I_frac_switch:
             returnval = []
